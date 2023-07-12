9e34f715748c872e5eb9dfedfe2a8eb61c747d0e
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-07-12 21:14:03 +0530

    add layout

---
+++ script.js
@@ -0,0 +1,16 @@
+// const radio_change = function () {
+//     const checked = document.querySelector('.upwrap .custom-radio-button input:checked');
+//     console.log(checked);
+//     const text = checked.parentElement.querySelector('.label_text').innerHTML;
+//     const pt = document.querySelector('.upwrap .period_text');
+//     // pt.innerHTML = text ? text : 'select period';
+
+// }
+
+// const inputs = document.querySelectorAll('.upwrap .custom-radio-button input')
+// //console.log(inputs);
+// inputs.forEach(function (input) {
+//     input.addEventListener('change', radio_change);
+// });
+
+// radio_change();
